4859bde1177670b177445ee11788e1f8e794287a
comanstefaniacatalina <comanstefania18@stud.ase.ro>
2022-05-08 14:20:57 +0300

    First routes

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
-const express=require('express');
-const cors=require('cors')
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const messagesRouter = require('./router/messagesRouter');
 
-const app=express();
+const app = express();
 app.use(cors());
 
-const port=process.env.PORT || 8080;
+// for parsing application/json
+app.use(bodyParser.json()); 
+// for parsing application/xwww-
+app.use(bodyParser.urlencoded({ extended: true }));
 
+// Define routes
+app.use("/messages", messagesRouter);
+const port = process.env.PORT || 8080;
+/*
 app.get('/', (req,res)=>{
     res.send('Te-ai ajuns faci site-uri')
-});
+});*/
 
-app.listen(port, () =>{
-    console.log(`Cloud computing app listen on port ${port}!`)
+
+app.listen(port, () => {
+  console.log(`Cloud computing app listen on port ${port}!`);
 });
